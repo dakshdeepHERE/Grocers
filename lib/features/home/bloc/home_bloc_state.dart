f9ba936c19d9c262613b4c507d65b95dd5f8d1cb
Daksha Deep <dakshadeep1234@gmail.com>
2023-08-13 18:21:06 +0530

    remove wrong bloc

--- lib/features/home/bloc/home_bloc_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'home_bloc_bloc.dart';
-
-@immutable
-sealed class HomeBlocState {}
-
-abstract class HomeActionState extends HomeBlocState {}
-
-class HomeBlocInitial extends HomeBlocState {}
