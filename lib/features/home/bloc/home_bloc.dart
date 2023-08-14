8292ababa0fcddc027ed99c38643b276e4d32730
Daksha Deep <dakshadeep1234@gmail.com>
2023-08-14 22:12:13 +0530

    product data model added

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:akshitboc/data/grocerry_data.dart';
+import 'package:akshitboc/features/home/models/home_product_data_model.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -8,11 +10,27 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
+    on<HomeInitialEvent>(homeInitialEvent);
     on<HomeProductWishlistButtonClickedEvent>(
         homeProductWishlistButtonClickedEvent);
     on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
     on<HomeWishlistButtonNavigateEvent>(homeWishlistButtonNavigateEvent);
     on<HomeCartButtonNavigateEvent>(homeCartButtonNavigateEvent);
+  }
+
+  FutureOr<void> homeInitialEvent(
+      HomeInitialEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoadingState());
+    await Future.delayed(Duration(seconds: 3));
+    emit(HomeLoadedSuccessState(
+        products: GrocerryData.groceryProducts
+            .map((e) => ProductDataModel(
+                name: e['name'],
+                category: e['category'],
+                price: e['price'],
+                quantity: e['quantity'],
+                imageURL: e['imageURL']))
+            .toList()));
   }
 
   FutureOr<void> homeProductWishlistButtonClickedEvent(
---
+++ lib/features/home/models/home_product_data_model.dart
@@ -0,0 +1,14 @@
+class ProductDataModel {
+  final String name;
+  final String category;
+  final double price;
+  final String quantity;
+  final String imageURL;
+
+  ProductDataModel(
+      {required this.name,
+      required this.category,
+      required this.price,
+      required this.quantity,
+      required this.imageURL});
+}
